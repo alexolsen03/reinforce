de349bf00613d6d060f99ca5f3a3fcc91f0df1cd
alex <alexolsen03@gmail.com>
2013-08-07 20:15:29 -0500

    my initial commit message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reinforce::Application.config.secret_key_base = 'ab0a5a76b52af8f968adabbaf6e2064032a4ee890af5ecf976d74c976c201cf0ce0dbd6b5b1c9262b064663fd6b9b6a6756127da63a0b94a2aa66ae438a555ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Reinforce::Application.config.session_store :cookie_store, key: '_reinforce_session'
